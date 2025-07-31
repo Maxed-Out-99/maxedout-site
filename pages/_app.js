309e2068e50b925b6d4ea7bcf8a3e57f77492403
Maxed-Out-99 <maxedout99999@gmail.com>
2025-07-30 19:38:51 -0700

    Add Geist font globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { GeistSans } from "next/font/google";
+
+const geist = GeistSans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={geist.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
